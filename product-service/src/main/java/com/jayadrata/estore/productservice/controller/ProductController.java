66b2bfa47d5484fbe858997381df11a29b5055d1
Jayadrata Middey <imjaya03@gmail.com>
2021-05-18 00:56:50 +0530

    discovery-service added

---
+++ product-service/src/main/java/com/jayadrata/estore/productservice/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.jayadrata.estore.productservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @PostMapping
+    public String createProduct(){
+        return "HTTP POST Handled";
+    }
+
+    @GetMapping
+    public String getProduct(){
+        return "HTTP GET Handled";
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        return "HTTP PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct(){
+        return "HTTP DELETE Handled";
+    }
+}
